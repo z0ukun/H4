464582d40525983827c427311ca7a285b5a54f97
z0ukun <z0ukun@163.com>
2018-11-21 22:40:19 +0800

    H4

--- booktest/admin.py
+++ booktest/admin.py
@@ -2,6 +2,29 @@
 from django.contrib import admin
 from models import *
 
+
+class HeroInfoInline(admin.StackedInline):
+    model = heroinfo
+    extra = 3
+
+
+class BookInfoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'btitle', 'bpub']
+    list_filter = ['btitle']
+    search_fields = ['id', 'btitle', 'bpub']
+    list_per_page = 2
+    fieldsets = [
+        ('base', {'fields': ['btitle']}),
+        ('super', {'fields': ['bpub']}),
+    ]
+    inlines = HeroInfoInline,
+
+
+class HeroInfoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'hname', 'hgender', 'hcontent', 'hbook']
+    list_filter = ['id', 'hname']
+
+
 # Register your models here.
-admin.site.register(bookinfo)
-admin.site.register(heroinfo)
+admin.site.register(bookinfo, BookInfoAdmin)
+admin.site.register(heroinfo, HeroInfoAdmin)
--- booktest/views.py
+++ booktest/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import *
+from .models import *
+
 
 # Create your views here.
+def index(request):
+    bookList = bookinfo.objects.all()
+    context = {'list': bookList}
+    return render(request, 'booktest/index.html', context)
+
+
+def show(request, id):
+    book = bookinfo.objects.get(pk=id)
+    herolist = book.heroinfo_set.all()
+    context = {'list': herolist}
+    return render(request, "booktest/show.html", context)
